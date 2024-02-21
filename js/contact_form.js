a2c9b4e66d776dde6bf14e900906afdcde5439e2
faisman97 <faisalsuleimanfarouk@gmail.com>
2024-02-21 17:43:02 +0100

    Up

--- js/contact_form.js
+++ js/contact_form.js
@@ -1,27 +1,25 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbyquMWpKJTGdDEIO__Pdc7DCLjPZWOm34LsXSS0Jdot-hSyaUieQIxJ8NPPyVsK9DYZ/exec';
-const form = document.forms['google-sheet'];
+    const scriptURL =
+        'https://script.google.com/macros/s/AKfycbyquMWpKJTGdDEIO__Pdc7DCLjPZWOm34LsXSS0Jdot-hSyaUieQIxJ8NPPyVsK9DYZ/exec';
+const form = document.getElementById('google-sheet');
 
 form.addEventListener('submit', e => {
     e.preventDefault();
 
-    // Get the hCaptcha response token
-    const hCaptchaResponse = document.getElementById('h-captcha-response').value;
-
-    // Validate hCaptcha token
-    if (!hCaptchaResponse) {
-        alert("Please complete the hCaptcha challenge.");
+    // Validate hCaptcha first
+    const hcaptchaResponse = form.querySelector('.h-captcha-response').value;
+    if (!hcaptchaResponse) {
+        alert('Please complete the captcha.');
         return;
     }
 
-    // If hCaptcha token is valid, submit the form
+    // If hCaptcha validation passes, proceed with form submission
     fetch(scriptURL, {
             method: 'POST',
             body: new FormData(form)
         })
         .then(response => {
             alert("Thank You! An Expert will contact you soon...");
-            // Reset the form after successful submission if needed
-            form.reset();
+            form.reset(); // Reset the form after successful submission
         })
         .catch(error => console.error('Error!', error.message));
-});
+}); 
