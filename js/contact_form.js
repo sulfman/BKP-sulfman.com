cc36dbbda530083964403be64da2e9a90d8c7363
faisman97 <faisalsuleimanfarouk@gmail.com>
2024-02-26 17:34:23 +0100

    15

--- js/contact_form.js
+++ js/contact_form.js
@@ -1,36 +1,12 @@
-document.getElementById('contactForm').addEventListener('submit', function (event) {
-    event.preventDefault();
-    var formData = new FormData(this);
-    sendData(formData);
-});
+ const scriptURL = '<https://script.google.com/macros/s/AKfycbx9SRbj9c7UXfhoMiSFwmfcMWOcZ1Jn3fC0UC8ok5FNm_xQqAqZxXC6D2L6j9yJLdzV9w/exec>'
+ const form = document.forms['contactForm']
 
-function sendData(formData) {
-    fetch('https://script.google.com/macros/s/AKfycbx9SRbj9c7UXfhoMiSFwmfcMWOcZ1Jn3fC0UC8ok5FNm_xQqAqZxXC6D2L6j9yJLdzV9w/exec', { // Replace 'your-apps-script-url' with the actual URL of your Google Apps Script endpoint
-            method: 'POST',
-            body: formData
-        })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.text();
-        })
-        .then(data => {
-            console.log(data);
-            showAlert('Form submitted successfully!', 'success');
-            document.getElementById('contactForm').reset(); // Reset the form after successful submission
-        })
-        .catch(error => {
-            console.error('There was an error!', error);
-            showAlert('An error occurred while submitting the form. Please try again later.', 'error');
-        });
-}
-
-function showAlert(message, type) {
-    swal({
-        title: '',
-        text: message,
-        icon: type,
-        button: 'OK',
-    });
-}
+ form.addEventListener('submit', e => {
+     e.preventDefault()
+     fetch(scriptURL, {
+             method: 'POST',
+             body: new FormData(form)
+         })
+         .then(response => console.log('Success!', response))
+         .catch(error => console.error('Error!', error.message))
+ })
